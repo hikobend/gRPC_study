ab06d36aaeb744b22086e769cf8558f8b55ebfa2
hikobend <hiko.bkend@gmail.com>
2023-02-16 12:14:33 +0900

    jsonに変換

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module coder
 go 1.19
 
 require google.golang.org/protobuf v1.28.1
+
+require github.com/golang/protobuf v1.5.2 // indirect
--- main.go
+++ main.go
@@ -3,10 +3,9 @@ package main
 import (
 	"coder/pb"
 	"fmt"
-	"io/ioutil"
 	"log"
 
-	"google.golang.org/protobuf/proto"
+	"github.com/golang/protobuf/jsonpb"
 )
 
 func main() {
@@ -26,28 +25,45 @@ func main() {
 		},
 	}
 
-	binData, err := proto.Marshal(employee)
+	// binData, err := proto.Marshal(employee)
+	// if err != nil {
+	// 	log.Fatalln("Can't serialize", err)
+	// }
+
+	// if err := ioutil.WriteFile("test.bin", binData, 0666); err != nil {
+	// 	log.Fatalln("Can't write", err)
+	// }
+
+	// in, err := ioutil.ReadFile("test.bin")
+	// if err != nil {
+	// 	log.Fatalln("Can't read", err)
+	// }
+
+	// // 空のEmployee構造体を用意
+	// readEmployee := &pb.Employee{}
+
+	// // デシリアライズされた結果がからの構造体の中に反映される。
+	// err = proto.Unmarshal(in, readEmployee)
+	// if err != nil {
+	// 	log.Fatalln("Can't read", err)
+	// }
+
+	// fmt.Println(readEmployee)
+
+	// JSONに変換
+
+	m := jsonpb.Marshaler{}
+	out, err := m.MarshalToString(employee)
 	if err != nil {
-		log.Fatalln("Can't serialize", err)
+		log.Fatalln("Can't marchal to json", err)
+	}
+	fmt.Println(out)
+
+	reamEmployee := &pb.Employee{}
+	if err := jsonpb.UnmarshalString(out, reamEmployee); err != nil {
+		log.Fatalln("can't unmarshal from json", err)
 	}
 
-	if err := ioutil.WriteFile("test.bin", binData, 0666); err != nil {
-		log.Fatalln("Can't write", err)
-	}
+	fmt.Println(reamEmployee)
 
-	in, err := ioutil.ReadFile("test.bin")
-	if err != nil {
-		log.Fatalln("Can't read", err)
-	}
-
-	// 空のEmployee構造体を用意
-	readEmployee := &pb.Employee{}
-
-	// デシリアライズされた結果がからの構造体の中に反映される。
-	err = proto.Unmarshal(in, readEmployee)
-	if err != nil {
-		log.Fatalln("Can't read", err)
-	}
-
-	fmt.Println(readEmployee)
 }
